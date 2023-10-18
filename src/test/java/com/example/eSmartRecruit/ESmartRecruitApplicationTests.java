14b46f4d3e3c0be3dc126674c91aaa71b10ef7ff
20110656@student.hcmute.edu.vn <20110656@student.hcmute.edu.vn>
2023-10-18 15:46:37 +0700

    Push code

---
+++ src/test/java/com/example/eSmartRecruit/ESmartRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eSmartRecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESmartRecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
