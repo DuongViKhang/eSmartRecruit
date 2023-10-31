1925550ac2893d587628e08cb7d438b3910b2348
tien <tienh333@gmail.com>
2023-10-31 13:33:19 +0700

    chinh sua thong tin user, xem thong tin user

--- src/main/java/com/example/eSmartRecruit/controllers/candidate/CandidateController.java
+++ src/main/java/com/example/eSmartRecruit/controllers/candidate/CandidateController.java
@@ -1,12 +1,15 @@
 package com.example.eSmartRecruit.controllers.candidate;
-
+import java.util.HashMap;
+import java.util.Map;
 
 import com.example.eSmartRecruit.config.ExtractUser;
 import com.example.eSmartRecruit.controllers.request_reponse.CandidateApplyResponse;
 import com.example.eSmartRecruit.controllers.request_reponse.OnePositionResponse;
+import com.example.eSmartRecruit.controllers.request_reponse.ResponseObject;
 import com.example.eSmartRecruit.models.Application;
 
 import com.example.eSmartRecruit.models.Position;
+import com.example.eSmartRecruit.models.User;
 import com.example.eSmartRecruit.services.impl.ApplicationService;
 import com.example.eSmartRecruit.services.IStorageService;
 import com.example.eSmartRecruit.services.impl.PositionService;
@@ -14,6 +17,7 @@ import com.example.eSmartRecruit.services.impl.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 
 import lombok.AllArgsConstructor;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -81,6 +85,46 @@ public class CandidateController {
             return new ResponseEntity<CandidateApplyResponse>(CandidateApplyResponse.builder().message(e.getMessage()).status("ERROR").build(),HttpStatus.NOT_IMPLEMENTED);
 
         }
+
+    }
+    //get user info
+    @GetMapping("/profile")
+    ResponseEntity<ResponseObject> getDetailUser(HttpServletRequest request) throws JSONException {
+        String authHeader = request.getHeader("Authorization");
+        //return new ResponseEntity<String>("hello",HttpStatus.OK);
+        ExtractUser userInfo = new ExtractUser(authHeader, userService);
+        if(!userInfo.isEnabled()){
+            return null;
+        }
+        Integer userId = userInfo.getUserId();
+        User user = userService.getUserById(userId);
+
+        Map<String, String> data = new HashMap<>();
+        data.put("username", user.getUsername());
+        data.put("email", user.getEmail());
+        data.put("phonename", user.getPhoneNumber());
+
+        return new ResponseEntity<ResponseObject>(ResponseObject.builder().status("Success").data(data).build(),HttpStatus.OK);
+
+    }
+    //update user
+    @PutMapping("/profile")
+    ResponseEntity<ResponseObject> updateUser(HttpServletRequest request,
+//                                              @RequestParam("email")String email,
+//                                              @RequestParam("phoneNumber")String phoneNumber
+                                              @RequestBody User user0
+                                              ) throws JSONException {
+        String authHeader = request.getHeader("Authorization");
+        //return new ResponseEntity<String>("hello",HttpStatus.OK);
+        ExtractUser userInfo = new ExtractUser(authHeader, userService);
+        if(!userInfo.isEnabled()){
+            return null;
+        }
+        Integer userId = userInfo.getUserId();
+        User user = userService.updateUser(User.builder()
+                .email(user0.getEmail())
+                .phoneNumber(user0.getPhoneNumber()).build(),userId);
+        return new ResponseEntity<ResponseObject>(ResponseObject.builder().status("Success").data(user).build(),HttpStatus.OK);
 
     }
 }
