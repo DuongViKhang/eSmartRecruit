1251aa6d96666fa331e1a2d2db4e61ce72a9a9d1
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2023-11-01 10:13:47 +0700

    sửa api chức năng xem hồ sơ đã nộp + xem chi tiết hồ sơ

--- src/main/java/com/example/eSmartRecruit/controllers/candidate/CandidateController.java
+++ src/main/java/com/example/eSmartRecruit/controllers/candidate/CandidateController.java
@@ -4,9 +4,12 @@ package com.example.eSmartRecruit.controllers.candidate;
 import com.example.eSmartRecruit.config.ExtractUser;
 import com.example.eSmartRecruit.controllers.request_reponse.CandidateApplyResponse;
 import com.example.eSmartRecruit.controllers.request_reponse.OnePositionResponse;
+import com.example.eSmartRecruit.controllers.request_reponse.ResponseObject;
 import com.example.eSmartRecruit.models.Application;
 
 import com.example.eSmartRecruit.models.Position;
+import com.example.eSmartRecruit.models.User;
+import com.example.eSmartRecruit.repositories.ApplicationRepos;
 import com.example.eSmartRecruit.services.impl.ApplicationService;
 import com.example.eSmartRecruit.services.IStorageService;
 import com.example.eSmartRecruit.services.impl.PositionService;
@@ -14,27 +17,13 @@ import com.example.eSmartRecruit.services.impl.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 
 import lombok.AllArgsConstructor;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
-import com.example.eSmartRecruit.models.Position;
-import com.example.eSmartRecruit.repositories.PositionRepos;
-import com.example.eSmartRecruit.services.impl.ApplicationService;
-import com.example.eSmartRecruit.services.IStorageService;
-import com.example.eSmartRecruit.services.impl.PositionService;
-import jakarta.servlet.http.HttpServletRequest;
-
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.sql.Date;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("eSmartRecruit/")
@@ -42,45 +31,72 @@ import java.util.List;
 public class CandidateController {
     private PositionService positionService;
     private ApplicationService applicationService;
+    private ApplicationRepos applicationRepository;
 
     private UserService userService;
     private IStorageService storageService;
-    @GetMapping("/home")
-    List<String> getAllCandidate(){
-        return List.of("Hello candidate","");
+
+    @GetMapping("/application")
+    public ResponseEntity<ResponseObject> getMyApplications(HttpServletRequest request) throws JSONException {
+        {
+            String authHeader = request.getHeader("Authorization");
+            //return new ResponseEntity<String>("hello",HttpStatus.OK);
+            ExtractUser userInfo = new ExtractUser(authHeader, userService);
+            if (!userInfo.isEnabled()) {
+                return null;
+            }
+            Integer userId = userInfo.getUserId();
+            User user = userService.getUserById(userId);
+
+            List<Application> applications = applicationRepository.findByCandidateID(user.getId());
+            List<Map<String, Object>> applicationList = new ArrayList<>();
+
+            for (Application app : applications) {
+                Map<String, Object> applicationMap = new HashMap<>();
+                applicationMap.put("applicationID", app.getId());
+                applicationMap.put("positionTitle", positionService.getSelectedPosition(app.getPositionID()).getTitle());
+                applicationMap.put("status", app.getStatus());
+                applicationMap.put("applicationDate", app.getCreateDate().toString());
+                applicationList.add(applicationMap);
+            }
+            Map<String, Object> data = new HashMap<>();
+            data.put("status", "success");
+            data.put("applications", applicationList);
+            return new ResponseEntity<ResponseObject>(ResponseObject.builder().status("Success").data(data).build(), HttpStatus.OK);
+
+        }
     }
 
-    @GetMapping("/position/{positionID}")
-    ResponseEntity<OnePositionResponse> getDetailPosition(@PathVariable("positionID")Integer id){
-        Position pos = positionService.getSelectedPosition(id);
-        //return new ResponseEntity<String>("hello",HttpStatus.OK);
-        return new ResponseEntity<OnePositionResponse>(OnePositionResponse.builder().status("SUCCESS").position(pos).build(),HttpStatus.OK);
-
-
-        //return new ResponseEntity<Positions>(positionService.getSelectedPosition(id),HttpStatus.OK);
-    }
-
-    @PostMapping("/application/create/{positionID}")
-    ResponseEntity<CandidateApplyResponse> applyForPosition(@PathVariable("positionID")Integer id, HttpServletRequest request, @RequestParam("cv")MultipartFile cv){
+    @GetMapping("/application/{applicationID}")
+    public ResponseEntity<ResponseObject> getApplicationDetails(@PathVariable("applicationID") Integer id, HttpServletRequest request) {
         try {
             String authHeader = request.getHeader("Authorization");
             ExtractUser userInfo = new ExtractUser(authHeader, userService);
-            if(!userInfo.isEnabled()){
-                return new ResponseEntity<CandidateApplyResponse>(CandidateApplyResponse.builder()
-                        .message("Account not active!").status("ERROR").build(),HttpStatus.BAD_REQUEST);
+            if (!userInfo.isEnabled()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
             }
-            String generatedFileName = storageService.storeFile(cv);
-            int candidateId = userInfo.getUserId();
+            Integer userId = userInfo.getUserId();
+            User user = userService.getUserById(userId);
 
-            Application application = new Application(candidateId, id, generatedFileName);
-
-            return new ResponseEntity<CandidateApplyResponse>(CandidateApplyResponse.builder()
-                    .message(applicationService.apply(application)).status("SUCCESS").build(),HttpStatus.OK);
-
-        }catch (Exception e){
-            return new ResponseEntity<CandidateApplyResponse>(CandidateApplyResponse.builder().message(e.getMessage()).status("ERROR").build(),HttpStatus.NOT_IMPLEMENTED);
-
+            Optional<Application> application = applicationRepository.findByIdAndCandidateID(id, user.getId());
+            if (application.isPresent()) {
+                Map<String, Object> data = new HashMap<>();
+                Application app = application.get();
+                data.put("status", "SUCCESS");
+                data.put("application", Map.of(
+                        "applicationID", app.getId(),
+                        "candidateName", user.getUsername(),
+                        "positionTitle", positionService.getSelectedPosition(app.getPositionID()).getTitle(),
+                        "status", app.getStatus(),
+                        "cv", "https://example.com/cv/",
+                        "applicationDate", app.getCreateDate().toString())
+                );
+                return ResponseEntity.ok(ResponseObject.builder().status("SUCCESS").data(data).build());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ResponseObject.builder().status("ERROR").build());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseObject.builder().message(e.getMessage()).status("ERROR").build());
         }
-
     }
 }
